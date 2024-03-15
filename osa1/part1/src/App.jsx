f5e26f289a65034b38b43d76eb369d903ea583ae
gitelras <ella.rasalo@helsinki.fi>
2024-03-15 14:25:22 +0200

    part1

---
+++ osa1/part1/src/App.jsx
@@ -0,0 +1,27 @@
+
+
+const App = () => {
+  const nimi = 'Pekkaaaa'
+  const ika = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  )
+}
+
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+export default App
