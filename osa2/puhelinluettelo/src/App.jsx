0e26cd0b7adc3c8ec9eda360416c2544635bcce0
gitelras <ella.rasalo@helsinki.fi>
2024-06-14 16:03:58 +0300

    2.8: puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const addName = (event) => {
     event.preventDefault()
-    if (newName.trim() === '') {
+    if (newName.trim() === '' || newNumber.trim() === '') {
+      alert('Both name and number are required')
       return
     }
 
@@ -23,10 +29,12 @@ const App = () => {
     }
 
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -37,13 +45,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map(person => 
-          <div key={person.name} style={{ margin: '0', padding: '0' }}>{person.name}</div>
+          <div key={person.name} style={{ margin: '0', padding: '0' }}>
+            {person.name} {person.number}
+          </div>
         )}
       </div>
     </div>
@@ -51,4 +64,3 @@ const App = () => {
 }
 
 export default App
-
